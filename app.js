175ec5dead8bdbf7b122980f3482db991aac4147
TahiaBushra <bushratahia@gmail.com>
2023-11-24 19:00:53 +0600

    Converted Option Done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // if and else if 
+
 const marks = 65;
 if (marks >= 80) {
     console.log("A+");
@@ -20,4 +21,22 @@ else if (marks >= 33) {
 }
 else {
     console.log("F");
-} 
+}
+
+// Converting Number Variables Into String
+
+const num = 50;
+const convertedNum = num.toString();
+console.log(typeof convertedNum);
+
+// Converting Number Variables Into String -  Option 2
+
+const number = 50;
+const convertedNumber = String(number);
+console.log(typeof convertedNum);
+
+// Converting String Variables Into Number
+
+const myName = "Tahia";
+const convertedName = Number(myName);
+console.log(typeof convertedName);
