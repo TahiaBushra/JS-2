c357a45e50edca425e73427bc8861112abafdbf8
TahiaBushra <bushratahia@gmail.com>
2023-11-24 19:37:43 +0600

    Operators Done

--- app.js
+++ app.js
@@ -40,3 +40,29 @@ console.log(typeof convertedNum);
 const myName = "Tahia";
 const convertedName = Number(myName);
 console.log(typeof convertedName);
+
+// Equality Operators
+
+console.log(50 == "50");
+console.log(50 === "50");
+
+// Operators (&&  || )
+
+const yourAge = 40;
+const bribe = 700000;
+const relativeOfGoverment = true;
+if ((yourAge <= 35 && bribe >= 500000) || relativeOfGoverment) {
+    console.log("You will get government job.");
+} else {
+    console.log("You won't get government job")
+}
+
+// Operators (&&  || ) - Example : 2
+
+const year = 2023;
+if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+    console.log("It's a leap year.");
+} else {
+    console.log("It's not a leap year.")
+}
+
